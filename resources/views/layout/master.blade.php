60d9e83b669279ef6b2a0983d00b95f03aff250a
Sabardi <Sabardibahari@gmail.com>
2024-03-17 20:02:50 +0700

    perbaikan

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <title>@yield('title')</title> -->
     <!-- jika lupa menuliskan nama title nya nilai defalut nya yg akan di jalankan -->
-    <title>@yield('title', Sistem Informasi)</title>
+    <title>@yield('title', 'Sistem Informasi')</title>
     <link rel="shortcut icon" href="/img/a.jpeg" type="image/x-icon">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
 
@@ -21,7 +21,7 @@
                     <a class="nav-link @yield('menuDosen')" href="/dosen">Data Dosen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link @yield('menuGalery')" href="/galery">Galery</a>
+                    <a class="nav-link @yield('menuGalery')" href="{{route('gambar')}}">Galery</a>
                 </li>
             </ul>
         </div>
